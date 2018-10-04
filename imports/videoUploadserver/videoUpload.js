9349dac0974e87bcd5928487506340e5628d83b7
poojagujar3 <poojagujar3@gmail.com>
2018-10-04 18:59:56 +0530

    Karishma resolved defects.

--- imports/videoUploadserver/videoUpload.js
+++ imports/videoUploadserver/videoUpload.js
@@ -53,11 +53,11 @@ if(s3Data)
                 allowClientCode: false,
                 chunkSize: 1024 * 1024,
                 onBeforeUpload: function(file) {
-                    if ( /mp4|3gp|ogv|webm/i.test(file.extension)) {
+                    if (file.size <= 209715200 && /mp4|3gp|ogv|webm/i.test(file.extension)) {
                         // limit size to 1GB and in mp4 format
                         return true;
                     } else {
-                        return "Please upload video of type .mp4, .3gp, .ogv or .webm";
+                        return "Please upload video of size less than 200 & of type .mp4, .3gp, .ogv or .webm";
                     }
 
                 },
