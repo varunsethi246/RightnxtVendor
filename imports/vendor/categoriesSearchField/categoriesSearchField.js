2c11293ead650917612007e85c4fa78027d52c3c
poojagujar3 <poojagujar3@gmail.com>
2018-12-12 19:05:36 +0530

    Karishma added changes.

--- imports/vendor/categoriesSearchField/categoriesSearchField.js
+++ imports/vendor/categoriesSearchField/categoriesSearchField.js
@@ -71,7 +71,7 @@ Template.categoriesSearchField.events({
 					}
 				}
 
-				if($('#alistCategory>.str-tags-each1').length<5){
+				if($('.str-tags-each1').length<5){
 					if(temp == 0){
 						selectedCategoriesList.push(data);	
 						$('#alistCategory').append("<div class='js-click-tag1 str-tags-each1' id='catgIndex-" + dataIndex + "' > <div class='str-category str-category1' > " + showData + " x </div> </div>");
@@ -198,7 +198,7 @@ Template.categoriesSearchField.events({
 						temp = 1;
 					}
 				}
-				if($('#alistCategory>.str-tags-each1').length<5){
+				if($('.str-tags-each1').length<5){
 					if(temp == 0){
 						selectedCategoriesList.push(text);	
 						$('#alistCategory').append("<div class='js-click-tag1 str-tags-each1' id='catgIndex-" + dataIndex + "' > <div class='str-category str-category1' > " + showData + " x </div> </div>");
